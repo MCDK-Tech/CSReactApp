cc9ce188e2316c82eb87cd06b4ef3ff90aebd7a7
MCDK-Tech <davidkim2023@u.northwestern.edu>
2022-10-01 12:33:54 -0500

    Style

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,7 +1,17 @@
+
+
 const Course = ({course}) => (
-    <div>
-      {course.term} CS {course.number}: {course.title}
+    <div className="card m-1 p-2">
+      <div className="card-body">
+        <h5 className="card-title">{course.term} CS {course.number}</h5>
+        <p className="card-text">{course.title}</p>
     </div>
+    <div className="card-footer bg-white">
+        <p className="text-center">{course.meets}</p>
+      </div>
+  </div>
+
+    
   );
-  
+
   export default Course;
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,6 +1,8 @@
 import Course from "./Course";
+import './Course.css'
+
 const CourseList = ({courses}) => (
-  <div>
+  <div className = "course_list">
     { Object.entries(courses).map(([id, course]) => <Course key={id} course={course} />) }
   </div>
 );
