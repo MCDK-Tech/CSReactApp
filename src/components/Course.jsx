e501f3305e21ffd68fd9592c7ff1bb95c7ea7437
MCDK-Tech <davidkim2023@u.northwestern.edu>
2022-11-07 22:22:03 -0600

    Content and Interaction Test

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -9,7 +9,7 @@ const using = () => {
 };
 
 const Course = ({id, course,  selected, unavailable, toggleSelected, profile}) => (
-    <div className="course card m-1 p-2" onClick={() => !selected.includes(id) && unavailable.includes(id)? '' : toggleSelected(id)}>
+    <div className="course card m-1 p-2" data-cy="course" onClick={() => !selected.includes(id) && unavailable.includes(id)? '' : toggleSelected(id)}>
       <div className={`card-body  ${selected.includes(id) ? 'selected' : unavailable.includes(id) ? 'unavailable': ''}`}>
         <h5 className="card-title">{course.term} CS {course.number}</h5>
         <p className="card-text">{course.title}</p>
